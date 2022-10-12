c6c2766d038bc9710a10768dc0abb7dcf42f11a6
DartPin <34026161+DartPin@users.noreply.github.com>
2022-10-12 09:17:21 +0300

    Fix install.sh
    Исправлена строка запуска docker compose container(убран пробел)

--- production/install.sh
+++ production/install.sh
@@ -23,6 +23,6 @@ docker load < ${HOME}/images/agc-aux-server.tar.gz
 docker load < ${HOME}/images/agc-aux-web.tar.gz
 
 out "Обновляем и запускаем последнию версию приложения"
-docker-compose up -d --no-deps --force-recreate --build agc-aux-server agc-aux-web
+docker compose up -d --no-deps --force-recreate --build agc-aux-server agc-aux-web
 
 out "Приложение успешно установлено и доступно на порту 8080"
